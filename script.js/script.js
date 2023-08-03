143bdcb874c2b31c318d0a258d702dd7d6cf77ad
SUSHMARAJENDRAN <141265769+SUSHMARAJENDRAN@users.noreply.github.com>
2023-08-03 22:17:26 +0530

    v4

---
+++ script.js/script.js
@@ -0,0 +1,49 @@
+(function(){
+const buttons  =  document.querySelectorAll('.countrBtn ')
+let count=0;
+ 
+buttons.forEach( function(buttons) {
+ buttons.addEventListener('click', function(){
+    if(buttons.classList.contains('prevBtn')){
+     count--;
+    }
+      else if(buttons.classList.contains('nextBtn')){
+        count++;
+      }
+      const counter=document.querySelector('#counter')
+      counter.textContent=count
+
+      if(count < 0){
+        counter.style.color = 'red'
+      }
+      else if (count > 0){
+        counter.style.color = 'green'
+      }
+      else{
+        counter.style.color = ' black'
+      }
+    })
+ 
+ 
+ 
+    })
+ 
+
+ 
+
+})()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
